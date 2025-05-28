b5e4d9407d439a64bbfb0016582ac48e15b8d2f4
Hollypie <hbriggs@byupathway.edu>
2025-05-27 19:49:53 -0600

    completed all the week 4 learning activiies.  The last one added is the server-side validation activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation')
 
 // Route to show login view
 router.get("/", utilities.handleErrors(accountController.buildLogin));
@@ -12,7 +13,12 @@ router.get("/", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 
-// Route to send registration info to Database
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router;
